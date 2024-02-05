6632107c40936a5e345905212e2368975dc56957
git-santanu <sddutta90@gmail.com>
2024-02-06 00:02:12 +0530

    up

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const heading = React.createElement(
   "h1",
   { id: "header" },
   "Hello world with ReactDom"
-); // {} --> allow to give attributes to tags/id
+); // {} --> allow to give attributes (tags/id)
 const root = ReactDOM.createRoot(document.getElementById("root")); // to render over there (letting the react to know my elements are render)
 root.render(heading);
 //React is a piece javaScript codes which can use at anywhere your app
 //  like where you create root it will work there only.
+// React.createElement =>  it is an Object => when it becomes render to DOM, it becomes HTML
